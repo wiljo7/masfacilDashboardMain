ed5afd21f6a627f9fd57bc7c59c6594ab3d4ff44
wiljo7 <wiljo7hdz@gmail.com>
2023-08-07 00:12:37 -0400

    somes

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ import os
 
 # Define la ruta completa al archivo requirements.txt
 if 1 ==1:
-    requirements_path = "masfacildashboardmainos/requirements.txt"#.path.join(os.getcwd(), "requirements.txt")
+    requirements_path = "masfacildashboardmain/requirements.txt"#.path.join(os.getcwd(), "requirements.txt")
     st.write(requirements_path)
     # Abre el archivo y lee las líneas
     try:
